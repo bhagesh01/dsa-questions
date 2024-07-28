8019350658da42949590792a43b83c9f5457f38e
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-07-28 13:31:52 +0530

    commiting scopes directory

---
+++ src/arrays/RotateArray.java
@@ -0,0 +1,10 @@
+package arrays;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// i know it is not the most optimised code but i am begining my dsa
+public class RotateArray {
+    public static void main(String[] args) {
+        System.out.println("try next time");
+    }
+}
