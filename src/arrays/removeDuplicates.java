bcae5e6e76b5bb5e9cd64b7b65d681b028d62ddc
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-08-16 22:33:21 +0530

    commiting scopes directory

--- src/arrays/removeDuplicates.java
+++ src/arrays/removeDuplicates.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class removeDuplicates {
     static int remove(int[] arr){
-        int n = arr.length;
+        int size = arr.length;
         int j=1;
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i < size; i++){
             if(arr[i] != arr[i - 1]){
                 arr[j] = arr[i];
                 j++;
