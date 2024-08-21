3a30374c6fe491b226987490dba92c6ede76348e
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-08-21 19:34:59 +0530

    commiting scopes directory

---
+++ src/Strings/CompressTheGivenString.java
@@ -0,0 +1,29 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CompressTheGivenString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the string: ");
+//        String s = sc.nextLine();
+
+
+
+//        Integer x = Integer.valueOf(23/10);
+        ArrayList<Integer> list = new ArrayList<>();
+//        int[] arr = {};
+//        int count = 123;
+//        int i =0;
+//        while(count > 0){
+//            int x = count%10;
+//            arr[i] = x;
+//            count /= 10;
+//            i++;
+//        }
+//        System.out.println(list);
+
+
+    }
+}
