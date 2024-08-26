773820137f1f09865c69ab199fc35f7826555887
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-08-26 21:32:58 +0530

    commiting scopes directory

---
+++ src/arrays/ProductOfArrayExceptSelf238.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class ProductOfArrayExceptSelf238 {
+    static void productOfArray(int[] arr){
+        int n = arr.length;
+        int[] newArr = new int[n];
+
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,6,5,7,8};
+
+
+    }
+}
