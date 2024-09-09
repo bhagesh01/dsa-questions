d60288cf86f38e6d742414d02ff3b782fa711a95
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-09-09 11:40:28 +0530

    commiting scopes directory

---
+++ src/Recursion/PrintNNumbers.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class PrintNNumbers {
+    static void printNumbers(int n){
+        if(n == 1){
+            System.out.println(1);
+            return;
+        }
+        System.out.println(n);
+        printNumbers(n-1);
+//        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        printNumbers(10);
+    }
+}
