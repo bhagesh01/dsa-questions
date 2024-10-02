f8befa07bb01110e057ead9eed3b104fe2132894
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-10-02 17:51:56 +0530

    commiting scopes directory

---
+++ src/arrays/countInversionsInArray.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class countInversionsInArray {
+    public static void main(String[] args) {
+//        Scanner sc  = new Scanner(System.in);
+//        System.out.println("Enter the size of the array: ");
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        System.out.println("Enter the elements of the array:");
+//        for(int i=0;i<n;i++){
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        int count=0;
+//        for(int i=0;i<n;i++){
+//            for(int j=i+1;j<n;j++){
+//                if(i<j && arr[i]>arr[j]){
+//                    count++;
+//                }
+//            }
+//        }
+//
+//
+//        System.out.println("the inversions are: " + count);
+
+//        int x = 2147483647;
+//        int y = -2147483647;
+//        int z = 2*x;
+//        int p = 2*y;
+
+//        System.out.println(z);
+//        System.out.println(p);
+
+
+
+        StringBuilder str1 = new StringBuilder("10373663");
+
+
+
+    }
+}
